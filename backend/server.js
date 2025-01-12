771ac0006e66e386eceaed05fdfdb9337c357653
manshikandu <manshikandu@gmail.com>
2025-01-12 21:33:34 +0545

    serverjs

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 app.use("/api/auth",authRoutes)
 app.listen(PORT, () =>{
 console.log("server is running on port http://localhost:" + PORT)
