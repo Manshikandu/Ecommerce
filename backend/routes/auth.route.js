02ddf42e1f9ea4b119efa8911d9c8d0981891663
manshikandu <manshikandu@gmail.com>
2025-01-12 20:27:36 +0545

    1.auth.route

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/signup",(req,res) =>{
+res.send("signup");
+    
+})
+
+router.get("/login",(req,res) =>{
+    res.send("signin");
+        
+ })
+
+    router.get("/logout",(req,res) =>{
+        res.send("logout");
+            
+ })
+export default router;
