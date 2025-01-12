0d5cf4021c82277c3b94f6966a5e3dbfd569021f
manshikandu <manshikandu@gmail.com>
2025-01-12 21:33:16 +0545

    feat/auth.controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,8 +1,26 @@
-export const signup = async (req,res) =>{
-res.send("signup");
-        
-}
+import User from "../models/user.model.js"
 
+export const signup = async (req,res) =>{
+const { name,email,password} = req.body;
+
+try {
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+    
+    return res.status(400).json({ message : "User already exists" })
+    
+    }
+    
+    const user = await User.create({name,email,password})
+    res.status(201).json({user,message : "User created sucessfully"});
+    
+ }   
+
+ catch (error) {
+   res.status(500).json({message: error.message});
+}
+}
 export const login = async (req,res) =>{
 res.send("signup");
             
@@ -14,4 +32,3 @@ res.send("signup");
 
 
 
- //coZFoL2QdlDSaWPd
