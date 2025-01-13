353f943ea97808c3d4fc1b3bd69fe27cd88d5535
manshikandu <manshikandu@gmail.com>
2025-01-14 02:10:00 +0545

    serverjs

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser  from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/product",productRoutes)
+app.use("/api/cart",cartRoutes)
+
 app.listen(PORT, () =>{
 console.log("server is running on port http://localhost:" + PORT)
 connectDB();
