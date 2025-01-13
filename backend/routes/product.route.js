60a0dc75092034e01fc744da7d2c6d621c755eb9
manshikandu <manshikandu@gmail.com>
2025-01-14 00:49:19 +0545

    2.product.route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getAllProducts } from "../controller/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts,getFeaturedProducts } from "../controller/product.controller.js";
 import { protectRoute,adminRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 
 router.get("/",protectRoute,adminRoute,getAllProducts)
+router.get("/featured",getFeaturedProducts)
+router.post("/",protectRoute,adminRoute,createProduct)
+router.delete("/:id",protectRoute,adminRoute,deleteProduct)
 export default router;
