46c8e7df38aa2590b08f184553ff4091ce8a18e9
manshikandu <manshikandu@gmail.com>
2025-01-13 13:07:45 +0545

    feat/auth.controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,39 @@
+import { redis } from "../lib/redis.js"
 import User from "../models/user.model.js"
+import jwt from "jsonwebtoken";
 
+
+
+const generateTokens = (userId) =>{
+const accessToken = jwt.sign({ userId}, process.env.ACCESS_TOKEN_SECRET,{
+   expiresIn: "15m",
+})
+
+const refreshToken = jwt.sign({ userId}, process.env.REFRESH_TOKEN_SECRET,{
+   expiresIn : "7d",
+})
+return {accessToken,refreshToken};
+}
+
+const storerefreshToken = async(userId,refreshToken) => {
+   await redis.set(`refresh_token:${userId}`,refreshToken,"EX",7*24*60*60)
+}
+
+const setCookies = (res,accessToken,refreshToken) => {
+   res.cookie("accessToken",accessToken,{
+   httpOnly : true,  //prevent xss attacks,cross site scripting attack
+   secure: process.env.NODE_ENV == "production",
+   sameSite : "strict", //prevents csrf attack,croos-site request forgery attack
+   maxAge: 15 * 60 * 10000,
+ })
+
+   res.cookie("refreshToken",refreshToken,{
+   httpOnly : true,  //prevent xss attacks,cross site scripting attack
+   secure: process.env.NODE_ENV == "production",
+   sameSite : "strict", //prevents csrf attack,croos-site request forgery attack
+   maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
+ })
+}
 export const signup = async (req,res) =>{
 const { name,email,password} = req.body;
 
@@ -13,7 +47,21 @@ try {
     }
     
     const user = await User.create({name,email,password})
-    res.status(201).json({user,message : "User created sucessfully"});
+
+    //authenticate
+    const {accessToken,refreshToken} = generateTokens(user._id)
+    await storerefreshToken(user._id,refreshToken)
+
+    setCookies(res,accessToken,refreshToken)
+    
+    res.status(201).json({user: {
+      _id : user._id,
+      name : user.name,
+      email : user.email,
+      role : user.role,
+    },
+    message : "User created sucessfully",
+   });
     
  }   
 
