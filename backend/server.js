9ea472eedc8d002c0cc16e2989b14b3bb46d56c0
manshikandu <manshikandu@gmail.com>
2025-01-13 23:42:14 +0545

    serverjs

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser  from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js"
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
+app.use("/api/product",productRoutes)
 app.listen(PORT, () =>{
 console.log("server is running on port http://localhost:" + PORT)
 connectDB();
